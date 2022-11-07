ffd67f374b0a1f394eca3b0dd23f1c32a7a45c04
bbosmedia <bbosmedia@users.noreply.github.com>
2022-11-07 11:34:12 +0500

    Footer links address adde

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,13 +17,13 @@ const Footer = () => {
 					<Link to="/">Home</Link>
 				</li>
 				<li className="footer-link-item">
-					<Link to="/">Projects</Link>
+					<Link to="/projects">Projects</Link>
 				</li>
 				<li className="footer-link-item">
-					<Link to="/">About</Link>
+					<Link to="/before-after">Before and After</Link>
 				</li>
 				<li className="footer-link-item">
-					<Link to="/">Contact</Link>
+					<Link to="/contact">Contact</Link>
 				</li>
 			</ul>
 			<ul className="footer-links">
@@ -37,22 +37,22 @@ const Footer = () => {
 					<Link to="/">#Dacha</Link>
 				</li>
 			</ul>
-			<div className='footer-last'>
+			<div className="footer-last">
 				<p className="footer-location">Komolon 30, Tashkent, Uzbekistan</p>
 				<span>
 					Mail for suppliers: <a href="mailto:abbos.nurgulshanov@mail.ru">abbos.nurgulshanov@mail.ru</a>
 				</span>
-                <ul className='footer-socials'>
-                    <li>
-                        <button onClick={()=>openWebTarget('https://www.facebook.com/')}>Facebook</button>
-                    </li>
-                    <li>
-                        <button onClick={()=>openWebTarget('https://www.instagram.com/')}>Instagram</button>
-                    </li>
-                    <li>
-                        <button onClick={()=>openWebTarget('https://www.telegram.com/')}>Telegram</button>
-                    </li>
-                </ul>
+				<ul className="footer-socials">
+					<li>
+						<button onClick={() => openWebTarget('https://www.facebook.com/')}>Facebook</button>
+					</li>
+					<li>
+						<button onClick={() => openWebTarget('https://www.instagram.com/')}>Instagram</button>
+					</li>
+					<li>
+						<button onClick={() => openWebTarget('https://www.telegram.com/')}>Telegram</button>
+					</li>
+				</ul>
 				<p>
 					Site made by <button onClick={() => openWebTarget('https://abbosfrelancer.netlify.app/')}>Abbos Nurgulshanov</button>
 				</p>
